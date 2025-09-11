7138e6b211c428d5f97782db1754be246f89c97f
Seu Nome <gustavo.moretti.souza@hotmail.com>
2025-09-11 20:37:38 -0300

    (Descrição das alterações)

--- src/main/java/com/projetoCRUD1/projetoEmpresa/SERVICE/EmpresaService.java
+++ src/main/java/com/projetoCRUD1/projetoEmpresa/SERVICE/EmpresaService.java
@@ -1,8 +1,31 @@
 package com.projetoCRUD1.projetoEmpresa.SERVICE;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.projetoCRUD1.projetoEmpresa.ENTITY.Empresa;
+import com.projetoCRUD1.projetoEmpresa.REPOSITORY.EmpresaRepository;
 
 @Service
 public class EmpresaService {
+
+    private EmpresaRepository empresaRepository;
+
+    public EmpresaService(EmpresaRepository ligacaoEmpresaRepository) {
+        empresaRepository = ligacaoEmpresaRepository;
+    }
+
+    public List<Empresa> findAll() {
+        return empresaRepository.findAll();
+    }//select * from empresa
+
+    public Empresa cadastrarEmpresa(Empresa dadosEmpresa){
+        return empresaRepository.save(dadosEmpresa);
+    }
+
+    public void deletarEmpresa(Empresa dadosEmpresa){
+        empresaRepository.delete(dadosEmpresa);
+    }
 
 }
