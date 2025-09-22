e909b51203928a9f9fc46e767d2aa652dbc19d7a
Seu Nome <gustavo.moretti.souza@hotmail.com>
2025-09-22 20:39:33 -0300

    alterações 22-09

--- src/main/java/com/projetoCRUD1/projetoEmpresa/CONTROLLER/EmpresaController.java
+++ src/main/java/com/projetoCRUD1/projetoEmpresa/CONTROLLER/EmpresaController.java
@@ -1,5 +1,17 @@
 package com.projetoCRUD1.projetoEmpresa.CONTROLLER;
 
+import org.springframework.stereotype.Controller;
+
+import com.projetoCRUD1.projetoEmpresa.SERVICE.EmpresaService;
+
+@Controller
 public class EmpresaController {
 
+    private final EmpresaService empresaService;
+
+    public EmpresaController(EmpresaService ligcaoEmpresaService) {
+        this.empresaService = ligcaoEmpresaService;
+    }
+
+    
 }
