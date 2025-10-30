9fd0d5204c11ba00726694c31f0076a3a0a27f58
Seu Nome <gustavo.moretti.souza@hotmail.com>
2025-10-30 20:37:32 -0300

    alteração 30-10

--- src/main/java/com/projetoCRUD1/projetoEmpresa/REPOSITORY/FuncionarioRepository.java
+++ src/main/java/com/projetoCRUD1/projetoEmpresa/REPOSITORY/FuncionarioRepository.java
@@ -1,5 +1,16 @@
 package com.projetoCRUD1.projetoEmpresa.REPOSITORY;
 
-public interface FuncionarioRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.projetoCRUD1.projetoEmpresa.ENTITY.Funcionario;
+
+@Repository
+public interface FuncionarioRepository extends
+        JpaRepository<Funcionario, Long> {
+
+    List<Funcionario> findByNomeContainingIgnoreCase(String nome_funcionario);
 
 }
